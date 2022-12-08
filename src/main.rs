08769a6e668d83e0561e659997c0ce78b32c5f30
Viktor Dick <vd@perfact.de>
2022-12-08 15:57:20 +0100

    Finish MVP

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate sdl2; 
 extern crate num;
 
+use std::cmp::min;
 use std::vec::Vec;
-use std::collections::VecDeque;
-
 use num::complex;
 
 use sdl2::pixels::Color;
@@ -16,8 +15,13 @@ use sdl2::gfx::primitives::DrawRenderer;
 
 const DIMX: i32 = 8;
 const DIMY: i32 = 6;
+const STEPS_TOTAL: i32 = 15;
+const STEPS_PAUSE: i32 = 5;
 
 type Complex = complex::Complex<i32>;
+type Direction = usize;
+type Owner = usize;
+
 const I : Complex = Complex::new(0, 1);
 
 // main directions
@@ -28,11 +32,20 @@ const DIRECTIONS: [Complex; 4] = [
     Complex::new(0, -1),
 ];
 
-const PLAYER_COUNT: usize = 2;
-const PLAYER_COLORS: [Color; PLAYER_COUNT] = [
-    Color::RGB(200, 0, 0),
-    Color::RGB(0, 200, 0),
-];
+struct Player {
+    started: bool,
+    alive: bool,
+    color: Color,
+}
+impl Player {
+    fn new(red: u8, green: u8, blue: u8) -> Player {
+        Player{
+            started: false,
+            alive: true,
+            color: Color::RGB(red, green, blue),
+        }
+    }
+}
 
 #[derive(Clone)]
 #[derive(Copy)]
@@ -41,26 +54,31 @@ struct Marble {
     pos: Complex,
     // target position we are moving to
     target: Complex,
-    owner: usize,
+    direction: Direction,
+    owner: Owner,
 }
 impl Marble {
-    fn new(pos: Complex, owner: usize) -> Marble {
+    fn new(pos: Complex, owner: Owner, direction: Direction) -> Marble {
         Marble {
             pos: pos,
             target: pos,
             owner: owner,
+            direction: direction,
         }
     }
 
     fn step(&mut self, remaining_steps: i32) {
+        if remaining_steps < STEPS_PAUSE {
+            return;
+        }
         self.pos = self.target
-            + ((self.pos - self.target) * remaining_steps) / (remaining_steps + 1);
+            + ((self.pos - self.target) * (remaining_steps-STEPS_PAUSE))
+            / (remaining_steps - STEPS_PAUSE + 1);
     }
 
-    fn draw(&self, canvas: &mut Canvas<Window>) -> Result<(), String> {
+    fn draw(&self, canvas: &mut Canvas<Window>, color: Color) -> Result<(), String> {
         let x = self.pos.re as i16;
         let y = self.pos.im as i16;
-        let color = PLAYER_COLORS[self.owner];
         canvas.aa_circle(x, y, 15, color)?;
         canvas.filled_circle(x, y, 15, color)?;
         Ok(())
@@ -72,14 +90,14 @@ struct Cell {
     coord: Complex,
     num_neighbors: usize,
     has_neighbor: [bool; 4],
-    // For each direction, we have a list (VecDeque) of marbles. Some of them might be "transient",
+    // For each direction, we have a vector of marbles. Some of them might be "transient",
     // i.e. not yet to be moved away in the current animation phase.
     // New marbles are added as non-transient.
     // At the start of the animation phase, we check if there are as many non-transient marbles as
     // there are neighbors - if so, we remove one from each direction and return it to the caller
     // (Grid), which adds them to the neighbors as transient marbles.
     // At the end of the animation phase, all marbles are marked as non-transient.
-    marbles: [VecDeque<Marble>; 4],
+    marbles: Vec<Marble>,
     num_transients: usize,
 }
 
@@ -87,17 +105,16 @@ impl Cell {
     fn new(coord: Complex) -> Cell {
         let has_neighbor = [
             coord.re < DIMX - 1,
-            coord.im > 0,
+            coord.im < DIMY-1,
             coord.re > 0,
-            coord.im < DIMY - 1,
+            coord.im > 0,
         ];
-        let x = VecDeque::with_capacity(3);
         Cell {
             owner: None,
             coord: coord,
             has_neighbor: has_neighbor,
             num_neighbors: has_neighbor.iter().map(|x| if *x {1} else {0}).sum(),
-            marbles: [x.clone(), x.clone(), x.clone(), x.clone()],
+            marbles: Vec::with_capacity(9),
             num_transients: 0,
         }
     }
@@ -107,23 +124,75 @@ impl Cell {
             // Set owner if it is not yet set, but return an error if it is set differently
             return Err(())
         }
+        let mut free = self.has_neighbor.clone();
+        for marble in self.marbles.iter() {
+            free[marble.direction] = false;
+        }
         for direction in 0..4 {
-            if !self.has_neighbor[direction] || !self.marbles[direction].is_empty() {
-                continue
+            if !free[direction] {
+                continue;
             }
             let coord = self.coord * 100 + Complex::new(50, 50) + 25*DIRECTIONS[direction];
-            self.marbles[direction].push_back(Marble::new(coord, owner));
+            self.marbles.push(Marble::new(coord, owner, direction));
             break;
         }
         Ok(())
     }
 
-    fn start_animation(&mut self) -> Vec<(usize, Marble)> {
-        // If all slots have marbles, push them together with their direction into the result and
-        // remove them from the cell itself. If any marble is found in the secondary slot of the
-        // corresponding direction, move it into the primary slot.
+    fn check_overflow(&mut self) -> Vec<Marble> {
+        // If all slots have marbles, push them into the result and remove them from the cell
+        // itself.
+        if self.marbles.len() - self.num_transients < self.num_neighbors {
+            return vec![]
+        }
         let mut result = Vec::with_capacity(4);
+        let mut todo = self.has_neighbor.clone();
+        let mut idx = 0;
+        while idx < self.marbles.len() {
+            let direction = self.marbles[idx].direction;
+            if !todo[direction] {
+                idx += 1;
+                continue;
+            }
+            todo[direction] = false;
+            result.push(self.marbles.remove(idx));
+        }
         result
+    }
+
+    fn target_direction(&self, origin: Direction) -> Direction {
+        // Strategy to find the direction where the marble should be facing:
+        // a) Do not use a direction that already has more marbles than any other or one where no
+        // neighbor exists
+        // b) Prefer opposite of the original direction, i.e. from where it came, then those
+        // neighboring that direction, then the original direction.
+        let mut count = [0; 4];
+        for existing in self.marbles.iter() {
+            count[existing.direction] += 1;
+        }
+        let mincount = count.iter()
+            .enumerate()
+            .filter(|(idx, _)| self.has_neighbor[*idx])
+            .fold(4, |acc, (_, x)| min(acc, *x));
+        let mut is_candidate = self.has_neighbor.clone();
+        for direction in 0..4 {
+            is_candidate[direction] &= count[direction] == mincount
+        }
+        for rotation in [2, 1, 3, 0] {
+            let direction = (origin+rotation)%4;
+            if is_candidate[direction] {
+                return direction;
+            }
+        }
+        0 //should not happen
+    }
+
+    fn receive(&mut self, mut marble: Marble) {
+        self.owner = Some(marble.owner);
+        marble.direction = self.target_direction(marble.direction);
+        marble.target = self.coord * 100 + Complex::new(50, 50) + 25*DIRECTIONS[marble.direction];
+        self.marbles.push(marble);
+        self.num_transients += 1;
     }
 
     fn draw(&self, canvas: &mut Canvas<Window>) -> Result<(), String> {
@@ -138,9 +207,6 @@ impl Cell {
             canvas.filled_circle(x, y, 15, Color::RGB(255, 255, 255))?;
             canvas.aa_circle(x, y, 15, Color::RGB(0, 0, 0))?;
         }
-        for marble in self.marbles.iter().flatten() {
-            marble.draw(canvas)?;
-        }
         Ok(())
     }
 }
@@ -153,13 +219,12 @@ enum State {
 struct Grid {
     cells: Vec<Cell>,
     state: State,
+    steps: i32,
     active_player: usize,
+    players: Vec<Player>,
 }
 
 impl Grid {
-    fn cell(&self, coordinates: Complex) -> &Cell {
-        &self.cells[(DIMY*coordinates.re + coordinates.im) as usize]
-    }
     fn cell_mut(&mut self, coordinates: Complex) -> &mut Cell {
         &mut self.cells[(DIMY*coordinates.re + coordinates.im) as usize]
     }
@@ -170,16 +235,42 @@ impl Grid {
                 cells.push(Cell::new(coordx+I*coordy))
             }
         }
+        let mut players = vec![];
+        players.push(Player::new(200, 0, 0));
+        players.push(Player::new(0, 200, 0));
+        players.push(Player::new(0, 0, 200));
         Grid{
             cells: cells,
             state: State::AcceptingInput,
+            steps: 0,
+            players: players,
             active_player: 0,
+        }
+    }
+
+    fn spread(&mut self, coord: Complex) -> bool {
+        let moving = self.cell_mut(coord).check_overflow();
+        if moving.is_empty() {
+            return false;
+        }
+        for marble in moving.into_iter() {
+            self.cell_mut(coord + DIRECTIONS[marble.direction]).receive(marble)
+        }
+        true
+    }
+
+    fn next_player(&mut self) {
+        loop {
+            self.active_player = (self.active_player + 1)%self.players.len();
+            if self.players[self.active_player].alive {
+                return
+            }
         }
     }
 
     fn click(&mut self, x: i32, y: i32) {
         match self.state {
-            State::AcceptingInput => { },
+            State::AcceptingInput => (),
             _ => return
         }
 
@@ -189,11 +280,62 @@ impl Grid {
             return
         }
         let active_player = self.active_player;
-        match self.cell_mut(x+y*I).add(active_player) {
+        self.players[active_player].started = true;
+        let coord = x+y*I;
+        let cell = self.cell_mut(coord);
+        match cell.add(active_player) {
             Ok(_) => {
-                self.active_player = (self.active_player + 1) % PLAYER_COUNT;
+                if self.spread(coord) {
+                    self.state = State::Animating;
+                    self.steps = STEPS_TOTAL;
+                } else {
+                    self.next_player();
+                }
             },
             Err(_) => {}
+        }
+    }
+
+    fn step(&mut self) {
+        match self.state {
+            State::Animating => (),
+            _ => return
+        };
+
+        for cell in self.cells.iter_mut() {
+            for marble in cell.marbles.iter_mut() {
+                marble.step(self.steps)
+            }
+        }
+        if self.steps > 0 {
+            self.steps -= 1;
+            return;
+        }
+        for player in self.players.iter_mut() {
+            player.alive = !player.started;
+        }
+        for cell in self.cells.iter_mut() {
+            for marble in cell.marbles.iter_mut() {
+                marble.owner = cell.owner.unwrap();
+            }
+            if cell.marbles.len() == 0 {
+                cell.owner = None;
+            } else {
+                self.players[cell.owner.unwrap()].alive = true;
+            }
+            cell.num_transients = 0;
+        }
+        let mut continuing = false;
+        for posx in 0..DIMX {
+            for posy  in 0..DIMY {
+                continuing |= self.spread(posx+I*posy);
+            }
+        }
+        if continuing {
+            self.steps = STEPS_TOTAL;
+        } else {
+            self.state = State::AcceptingInput;
+            self.next_player();
         }
     }
     
@@ -206,17 +348,26 @@ impl Grid {
         for i in 0..DIMY {
             canvas.hline(0, (100*DIMX) as i16, (i*100) as i16, Color::RGB(0, 0, 0))?;
         }
-        for player in 0..PLAYER_COUNT {
+        for (idx, player) in self.players.iter().enumerate() {
             let x = (DIMX * 100 + 50) as i16;
-            let y = (30 + player as i16 * 40) as i16;
-            canvas.filled_circle(x, y, 15, PLAYER_COLORS[player])?;
+            let y = (30 + idx * 40) as i16;
+            canvas.filled_circle(x, y, 15, player.color)?;
             canvas.aa_circle(x, y, 15, Color::RGB(0, 0, 0))?;
-            if player == self.active_player {
+            if idx == self.active_player {
                 canvas.filled_pie(x-20, y, 20, 160, 200, Color::RGB(0, 0, 0))?;
+            }
+            if !player.alive {
+                canvas.thick_line(x-15, y-15, x+15, y+15, 2, Color::RGB(0, 0, 0))?;
+                canvas.thick_line(x-15, y+15, x+15, y-15, 2, Color::RGB(0, 0, 0))?;
             }
         }
         for cell in self.cells.iter() {
             cell.draw(canvas)?;
+        }
+        for cell in self.cells.iter() {
+            for marble in cell.marbles.iter() {
+                marble.draw(canvas, self.players[marble.owner].color)?;
+            }
         }
         Ok(())
     }
@@ -255,6 +406,7 @@ pub fn main() -> Result<(), String> {
                 _ => {}
             }
         }
+        grid.step();
         grid.draw(&mut canvas)?;
         canvas.present();
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
